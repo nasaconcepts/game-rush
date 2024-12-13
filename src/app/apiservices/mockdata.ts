45f445a06858896535af080275a8d18a1a683df4
Chinasa Obichere <chinasaobichere@admins-Air.broadband>
2024-12-13 00:02:12 +0000

    adding mock data

--- src/app/apiservices/mockdata.ts
+++ src/app/apiservices/mockdata.ts
@@ -12,27 +12,26 @@ export const mockdata = {
       quizVideo: 'https://www.youtube.com/shorts/ij6jwjizw54',
       quizImage:
         'https://www.cartrollers.com/wp-content/uploads/2024/08/Three-Crowns-Filled-Evaporated-Milk-160g-24-Tins-390x390.jpeg',
-      quizMessage: 'Why does World like war',
+      quizMessage: 'When did America gain their Independence',
       optionType: 'single',
 
       options: [
-        { optionId: 12, answerText: '1976' },
-        { optionId: 13, answerText: '1966' },
-        { optionId: 14, answerText: '1990' },
+        { optionId: 123, answerText: '22 July 1970' },
+        { optionId: 124, answerText: '4 July 1776' },
+        { optionId: 127, answerText: '2 January 2020' },
 
-        { optionId: 125, answerText: '1966' },
+        { optionId: 125, answerText: '20 March 1966' },
       ],
     },
     {
       quizId: 1202,
-      quizMessage: 'When did World War II stopped',
+      quizMessage: 'Which of these are sea animals',
       optionType: 'multiple',
       options: [
-        { optionId: 123, answerText: '1970' },
-        { optionId: 124, answerText: '1963' },
-        { optionId: 127, answerText: '1961' },
-
-        { optionId: 125, answerText: '1966' },
+        { optionId: 133, answerText: 'Goat' },
+        { optionId: 134, answerText: 'Shark' },
+        { optionId: 137, answerText: 'Tiger' },
+        { optionId: 135, answerText: 'Crab' },
       ],
     },
   ],
